1a6d9d3f62d2c62b6e824f27c351e164ebac115c
BernadDwiki <125329942+BernadDwiki@users.noreply.github.com>
2026-02-26 15:00:00 +0700

    Add files via upload

---
+++ minitask-7.js
@@ -0,0 +1,20 @@
+const nilai = {
+    matematika : 90,
+    bahasa_inggris : 80,
+    bahasa_indonesia : 85,
+    ipa : 90
+}
+
+function hitungNilai(nilai){
+    const {matematika, bahasa_inggris, bahasa_indonesia, ipa}= nilai
+    let nilai_total = matematika+bahasa_inggris+bahasa_indonesia+ipa
+    let rata_rata = nilai_total/4
+
+    const hasil =[nilai_total, rata_rata]
+    return hasil
+}
+
+const hasil = hitungNilai(nilai)
+
+console.log("Nilai Total : "+hasil[0])
+console.log("Rata - rata : "+hasil[1])
